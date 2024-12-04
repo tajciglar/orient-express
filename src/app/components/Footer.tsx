b334cd85507d28274f23ef3cdc0cb8e9e35f5785
tajciglar <taj.brzovic@gmail.com>
2024-12-04 11:45:21 +0100

    Skeleton structure setup

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <p>&copy; {new Date().getFullYear()} Orient Express samopostrežna restavracija. Vse pravice zadržane.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+import Footer from "./components/footer";
+import Header from "./components/header";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Orient Express</h1>
-    </div>
+    <> 
+    <Header></Header>
+      <main className="text-center p-6">
+      <h1 className="text-4xl font-bold">Restavracija Orient Express</h1>
+      <p className="mt-4 text-lg">Check out this week&apos;s menu below!</p>
+      
+      <section className="mt-8">
+        <h2 className="text-2xl font-semibold">This Week&apos;s Menu</h2>
+        <ul className="mt-4 space-y-2">
+          <li>🍔 Cheeseburger</li>
+          <li>🍕 Margherita Pizza</li>
+          <li>🥗 Caesar Salad</li>
+        </ul>
+      </section>
+    </main>
+    <Footer></Footer>
+    </>
   );
 }
